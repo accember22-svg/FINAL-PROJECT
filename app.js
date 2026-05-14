52f4e787fb3b3a6b992f6e945e563fdc537d75f6
accember22-svg <accember22@gmail.com>
2026-05-14 10:53:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const collection = [  { Game: "Resident Evil 7: Biohazard", MC: "Ethan winters", gamenumber: 7 }, 
+   { Game: "Resident Evil Requiem", MC: "Grace Ashcroft", gamenumber: 9 },
+   { Game: "Resident Evil 4", MC: "Leon Kennedy", gamenumber: 4 },
+   { Game: "Resident Evil 0", MC: "Rebecca Chambers", gamenumber: 0 },
+    ,];
+function displaycollectl() {
+const displaycontainer = document.getElementById("collection-display");
+displaycontainer.innerHTML = "";
+
+
+}
